0c4b7a3a3123160cc2cf02663688092353401ec7
TejasParse <tejasparse1@gmail.com>
2022-12-06 22:24:01 +0530

    PDF Dataset getting read

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -3,33 +3,40 @@
 import PyPDF4
 from nltk.corpus import stopwords
 
+import pdfplumber
+
 def tokenize(path):
     # open PDF
-    pdfFileObj = open(path, 'rb')
-    pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
+    # pdfFileObj = open(path, 'rb')
+    # pdfReader = PyPDF4.PdfFileReader(pdfFileObj)
     stopword_list = list(stopwords.words("english"))
 
-    # read PDF file in a list
     pdf_content = []
-    for i in range(pdfReader.numPages):
-        pageObj = pdfReader.getPage(i)
-        pdf_content.append(pageObj.extractText())
 
-    # tokenize all the words in the resume
-    tokenize = []
-    for line in pdf_content:
-        tokenize = filter(None,(line.split(" ")))
+    with pdfplumber.open(rf'{path}') as pdf:
+        
+        first_page = pdf.pages[1]
+        
+    
+        for i in range(len(pdf.pages)):
+            pageObj = pdf.pages[i]
+            pdf_content.append(pageObj.extract_text())
 
-    # remove punctuations and case-fold
-    no_punctuations = []
-    for token in tokenize:
-        no_punctuations.append(token.rstrip(",:|.-").lower())
+        
+        tokenize = []
+        for line in pdf_content:
+            tokenize = filter(None,(line.split(" ")))
 
-    # remove stop words
-    without_stop_words = []
+        
+        no_punctuations = []
+        for token in tokenize:
+            no_punctuations.append(token.rstrip(",:|.-").lower())
 
-    for word in filter(None, no_punctuations):
-        if word not in stopword_list:
-            without_stop_words.append(word)
+       
+        without_stop_words = []
 
-    return without_stop_words
+        for word in filter(None, no_punctuations):
+            if word not in stopword_list:
+                without_stop_words.append(word)
+
+        return without_stop_words
